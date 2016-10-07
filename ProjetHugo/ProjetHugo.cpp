8aa089a5855afbc393dd96062bba014153223461
Olivier Lafleur <olivier.lafleur@cll.qc.ca>
2016-10-07 11:29:35 -0400

    Main

--- ProjetHugo/ProjetHugo.cpp
+++ ProjetHugo/ProjetHugo.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
+#include "validationFormat.h"
 
+using namespace std;
 
 int main()
 {
-    return 0;
+	bool resultatImmatriculation = validerImmatriculation("627 XNN");
+
+	if (resultatImmatriculation) {
+		cout << "Immatriculation valide\n";
+	}
+	else {
+		cout << "Immatriculation non valide\n";
+	}
+
+	bool resultatNiv = validerNiv("11111111111111111");
+
+	if (resultatNiv) {
+		cout << "NIV valide\n";
+	}
+	else {
+		cout << "NIV invalide\n";
+	}
+	
+	int bonjour;
+
+	cin >> bonjour;
 }
 
